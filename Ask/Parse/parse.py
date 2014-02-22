5a12aa905699013a98c3f9da4534952774d13a2e
akshayn1107 <ananavat@andrew.cmu.edu>
2014-02-22 15:30:34 -0500

    added comments + author

--- Ask/Parse/parse.py
+++ Ask/Parse/parse.py
@@ -1,6 +1,11 @@
-#right now all it does is returun a line by line list of the file
-#contents (where each line is a space seperated list of strings. 
-# We probably will want a better parser later
+# parse.py
+# author: Akshay Nanavati
+#
+# implements a basic parser for wiki articles.
+# right now all it does is returun a line by line list of the file
+# contents (where each line is a space seperated list of strings. 
+# we probably will want a better parser later
+
 def parse(fname):
     f = open(fname)
     ret = []
