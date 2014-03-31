bda59f97f1aad8fda55f8fa9888da84082cd6982
akshayn1107 <akshay.nanavati1@gmail.com>
2014-03-31 15:12:54 -0400

    updated easy question generation

--- Ask/Parse/parse.py
+++ Ask/Parse/parse.py
@@ -6,10 +6,22 @@
 # contents (where each line is a space seperated list of strings. 
 # we probably will want a better parser later
 
-def parse(fname):
-    f = open(fname)
-    ret = []
-    for line in f:
-        ret = ret + [line.split(" ")]
-    f.close()
-    return ret    
+import subprocess, socket, nltk
+
+def parse_sentence(sentence):
+    host = socket.gethostname()
+    port = 5557
+    s = socket.socket()
+    s.connect((host, port))
+    s.send(sentence + "\n")
+    data = ""
+    while (len(data) == 0) or (data[-1] != "\n"):
+        data += s.recv(1024)
+    ret = data.split("\n")[0]
+    t = nltk.tree.Tree(ret)
+    return t
+
+def parse(text):
+    import nltk.data
+    tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+    return [parse_sentence(s) for s in tokenizer.tokenize(text)]
