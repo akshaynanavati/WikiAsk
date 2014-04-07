b86efbd3238e91af5d6131bdd8c13e7a49ab2f06
arjunkar <arjunkar@andrew.cmu.edu>
2014-04-07 00:22:09 -0400

    Observed 'where' implies PP

--- Answer/answer_where.py
+++ Answer/answer_where.py
@@ -1,11 +1,25 @@
 import nltk
+from stat_parser import Parser
+
+def get_prep(tree):
+    if hasattr(tree,"node") and tree.node:
+        if tree.node == "PP":
+            return [tree.flatten()]
+        else:
+            pps = []
+            for child in tree:
+                pps.extend(get_prep(child))
+        return pps
+    return []
 
 def get_location(sent, f):
-    known = sorted(f.entities["LOCATION"].items(), key = lambda x: x[0],
-            reverse = True)
-    for entity in known:
-        if entity[0] in sent:
-            return entity[0]
+    p = Parser()
+    print sent.nes
+    if len(sent.nes["LOCATION"]) > 0:
+        tree = p.parse(sent.raw)
+        preps = get_prep(tree)
+        preps = [' '.join(x.leaves()) for x in preps]
+        return ' '.join(preps)
     return None
 
 def answer(quest, ml, f):
