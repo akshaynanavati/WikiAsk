f2bc16d13d29eea3f61d275b934df1795975e449
philiptmassey <philip.t.massey@gmail.com>
2014-04-07 00:24:29 -0400

    Added parsetree to Sentence object

--- Answer/finder.py
+++ Answer/finder.py
@@ -17,9 +17,13 @@ class Sentence:
         self.lemmas = []
         self.nes = {}
         self.corefs = {}
+        self.parsetree = None
 
     def __repr__(self):
-        return "Sentence Object\nRaw: %s\nWords: %s\nPOS: %s\nLemmas: %s\nNEs: %s\nCorefs: %s\n" % (self.raw, str(self.words), str(self.pos), str(self.lemmas), str(self.nes), str(self.corefs))
+        return "Sentence Object\nRaw: %s\nWords: %s\nPOS: %s\nLemmas: %s\nNEs: %s\nCorefs: %s\nTree: %s\n"
+                % (self.raw, str(self.words), str(self.pos), 
+                str(self.lemmas), str(self.nes), str(self.corefs),
+                self.parsetree)
 
 class Finder:
 
@@ -70,6 +74,7 @@ class Finder:
         for sent in parse["sentences"]:
             s = Sentence()
             s.raw = sent["text"]
+            s.parsetree = sent["parsetree"]
             for word in sent["words"]:
                 s.words.append(word[0])
                 s.pos.append(word[1]["PartOfSpeech"])
