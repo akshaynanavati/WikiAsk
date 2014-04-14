75be10a9d86d8fe56204548ca8ea3197eec8cead
Akshay Nanavati <ananavat@unix3.andrew.cmu.edu>
2014-04-14 17:21:14 -0400

    cleaned up the ask repo

--- Ask/Generate/which.py
+++
@@ -1,9 +0,0 @@
-import lib
-
-def generate(npvp):
-    pruned = lib.prune(npvp, "SBAR")
-    cci = lib.find_index(pruned, "CC")
-    prunei = cci
-    leaves = pruned.leaves()[:prunei]
-    return ["which"] + leaves[1:]
-    
